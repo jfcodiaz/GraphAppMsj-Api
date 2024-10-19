312102906849d66cf354fe591ab726f658b6d196
JFcoDiaz <jfcodiaz@gmail.com>
2024-10-19 18:46:16 +0000

    🔧 [#2] Install and configure Apollo Server 🖥️

--- code/index.js
+++ code/index.js
@@ -1,12 +1,34 @@
 const express = require('express');
+const { ApolloServer, gql } = require('apollo-server-express');
 
 const app = express();
 const port = 3000;
+
+const typeDefs = gql`
+  type Query {
+    hello: String
+  }
+`;
+
+const resolvers = {
+  Query: {
+    hello: () => 'Hello, world!',
+  },
+};
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
 });
 
-app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`);
-});
+const server = new ApolloServer({ typeDefs, resolvers });
+
+async function startServer() {
+    await server.start();
+    server.applyMiddleware({ app });
+    app.listen({ port }, () => {
+        console.log(`xxExample app listening at http://localhost:${port}`);
+        console.log(`🚀 Server ready at http://localhost:${port}${server.graphqlPath}`)
+    });
+  }
+
+  startServer();
